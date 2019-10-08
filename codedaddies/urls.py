f2e930857573bdabf967315ef0929b490fd35d04
Vidali-Espisato <manish.11612939@lpu.in>
2019-10-09 04:30:56 +0530

    Added FrontEnd and requirements

--- codedaddies/urls.py
+++ codedaddies/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('app/', include('my_app.urls')),
+    path('', include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    context_items = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', context_items)
