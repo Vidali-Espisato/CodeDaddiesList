1fd62c8d7978ef24a0a71aa64ff36b202b88fd35
Vidali-Espisato <manish.11612939@lpu.in>
2019-10-10 16:51:36 +0530

    Final touches

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,15 @@
+import requests
+
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+from . import models
+# to turn spaces into proper url format ++++
+from requests.compat import quote_plus
 
 # Create your views here.
+
+BASE_CRAIGSLIST_URL = 'https://delhi.craigslist.org/search/sss?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 def home(request):
@@ -9,7 +18,38 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    response = requests.get(BASE_CRAIGSLIST_URL.format(quote_plus(search)))
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_listings = []
+    for post in post_listings:
+        # post_title = post.find(class_='result-title').text
+        # post_url = post.find('a').get('href')
+        # if post.find(class_='result-price'):
+        #     post_price = post.find(class_='result-price').text
+        # else:
+        #     post_price = "N/A"
+
+        # final_listings.append((post_title, post_url, post_price))
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image = BASE_IMAGE_URL.format(post.find(
+                class_='result-image').get('data-ids').split(',')[0].split(':')[1])
+        else:
+            post_image = 'https://dubsism.files.wordpress.com/2017/12/image-not-found.png'
+
+        final_listings.append(
+            (post.find(class_='result-title').text,
+             post.find('a').get('href'),
+             post.find(class_='result-price').text,
+             post_image),
+        )
+
     context_items = {
         'search': search,
+        'final_listings': final_listings,
     }
     return render(request, 'my_app/new_search.html', context_items)
